ebab10b8a1072bd3cd1586b8306635c2898e2a35
Fernando Medina <f.medina@paisanoscreando.com>
2020-09-20 03:44:14 -0300

    layout init

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import "./App.scss";
 const App = () => {
-  return <div>Hello from App.tsx</div>;
+  return (
+    <div className="App">
+      <div className="Header">Aca iria el puntaje</div>
+      <div className="Body">Body</div>
+    </div>
+  );
 };
 
 export default App;
